bd95552d37550b6813a3a2333f05c83b22eb5dd8
GuiSales21 <112443643+GuiSales21@users.noreply.github.com>
2022-08-29 23:57:08 -0300

    Add files via upload

---
+++ index.r
@@ -0,0 +1,10 @@
+#guilherme de Oliveira Sales
+#22207467
+
+library(dslabs)
+data("movielens")
+length(movielens$movieId)
+length(movielens)
+class(movielens$title)
+class(movielens$genres)
+length(levels(movielens$genres))
